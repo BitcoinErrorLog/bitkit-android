6eaf21ce6c9cea951873e2910cf18efad33d3fda
Ovi Trif <ovitrif@proton.me>
2025-08-05 17:19:18 +0200

    feat: fee items ui

--- app/src/main/java/to/bitkit/models/TransactionSpeed.kt
+++ app/src/main/java/to/bitkit/models/TransactionSpeed.kt
@@ -1,6 +1,8 @@
 package to.bitkit.models
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
@@ -62,5 +64,39 @@ fun TransactionSpeed.transactionSpeedUiText(): String {
         is TransactionSpeed.Medium -> stringResource(R.string.settings__fee__normal__value)
         is TransactionSpeed.Slow -> stringResource(R.string.settings__fee__slow__value)
         is TransactionSpeed.Custom -> stringResource(R.string.settings__fee__custom__value)
+    }
+}
+
+enum class FeeRate {
+    FAST, NORMAL, SLOW, CUSTOM;
+
+    @Composable
+    fun uiTitle(): String {
+        return when (this) {
+            FAST -> stringResource(R.string.fee__fast__title)
+            NORMAL -> stringResource(R.string.fee__normal__title)
+            SLOW -> stringResource(R.string.fee__slow__title)
+            CUSTOM -> stringResource(R.string.fee__custom__title)
+        }
+    }
+
+    @Composable
+    fun uiDescription(): String {
+        return when (this) {
+            FAST -> stringResource(R.string.fee__fast__description)
+            NORMAL -> stringResource(R.string.fee__normal__description)
+            SLOW -> stringResource(R.string.fee__slow__description)
+            CUSTOM -> stringResource(R.string.fee__custom__description)
+        }
+    }
+
+    @Composable
+    fun uiIcon(): Painter {
+        return when (this) {
+            FAST -> painterResource(R.drawable.ic_speed_fast)
+            NORMAL -> painterResource(R.drawable.ic_speed_normal)
+            SLOW -> painterResource(R.drawable.ic_speed_slow)
+            CUSTOM -> painterResource(R.drawable.ic_settings)
+        }
     }
 }
--- app/src/main/java/to/bitkit/ui/screens/wallets/send/SendFeeRateScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/send/SendFeeRateScreen.kt
@@ -1,26 +1,45 @@
 package to.bitkit.ui.screens.wallets.send
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import to.bitkit.R
+import to.bitkit.models.FeeRate
+import to.bitkit.models.PrimaryDisplay
+import to.bitkit.ui.LocalCurrencies
+import to.bitkit.ui.components.BodyMSB
+import to.bitkit.ui.components.BodySSB
 import to.bitkit.ui.components.BottomSheetPreview
-import to.bitkit.ui.components.Display
 import to.bitkit.ui.components.FillHeight
+import to.bitkit.ui.components.FillWidth
+import to.bitkit.ui.components.HorizontalSpacer
+import to.bitkit.ui.components.MoneyMSB
+import to.bitkit.ui.components.MoneySSB
 import to.bitkit.ui.components.PrimaryButton
 import to.bitkit.ui.components.VerticalSpacer
 import to.bitkit.ui.components.settings.SectionHeader
 import to.bitkit.ui.scaffold.SheetTopBar
 import to.bitkit.ui.shared.modifiers.sheetHeight
+import to.bitkit.ui.shared.util.clickableAlpha
 import to.bitkit.ui.shared.util.gradientBackground
 import to.bitkit.ui.theme.AppThemeSurface
+import to.bitkit.ui.theme.Colors
 import to.bitkit.viewmodels.SendUiState
 
 @Composable
@@ -51,20 +70,102 @@ private fun Content(
             .testTag("speed_screen")
     ) {
         SheetTopBar(stringResource(R.string.wallet__send_fee_speed), onBack = onBack)
-        Column(
+        SectionHeader(
+            title = stringResource(R.string.wallet__send_fee_and_speed),
             modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        FeeItem(
+            feeRate = FeeRate.FAST,
+            sats = 1790,
+            isSelected = false,
+            isDisabled = false,
+            onClick = {},
+        )
+        FeeItem(
+            feeRate = FeeRate.NORMAL,
+            sats = 1350,
+            isSelected = true,
+            isDisabled = false,
+            onClick = {},
+        )
+        FeeItem(
+            feeRate = FeeRate.SLOW,
+            sats = 850,
+            isSelected = false,
+            isDisabled = true,
+            onClick = {},
+        )
+        FeeItem(
+            feeRate = FeeRate.CUSTOM,
+            sats = 10,
+            isSelected = false,
+            isDisabled = false,
+            onClick = {},
+        )
+
+        FillHeight(min = 16.dp)
+
+        PrimaryButton(
+            text = stringResource(R.string.common__continue),
+            onClick = onContinue,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .testTag("continue_btn")
+        )
+        VerticalSpacer(16.dp)
+    }
+}
+
+@Composable
+private fun FeeItem(
+    feeRate: FeeRate,
+    sats: Long,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isSelected: Boolean = false,
+    isDisabled: Boolean = false,
+    unit: PrimaryDisplay = LocalCurrencies.current.primaryDisplay,
+) {
+    val color = if (isDisabled) Colors.Gray3 else MaterialTheme.colorScheme.primary
+    val accent = if (isDisabled) Colors.Gray3 else MaterialTheme.colorScheme.secondary
+    Column(
+        modifier = modifier
+            .clickableAlpha(onClick = onClick)
+            .then(
+                if (isSelected) Modifier.background(Colors.White06) else Modifier
+            ),
+    ) {
+        HorizontalDivider(Modifier.padding(horizontal = 16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .height(90.dp)
         ) {
-            SectionHeader(stringResource(R.string.wallet__send_fee_and_speed))
-            Display("TODO")
-
-            FillHeight(min = 16.dp)
-
-            PrimaryButton(
-                text = stringResource(R.string.common__continue),
-                onClick = onContinue,
-                modifier = Modifier.testTag("continue_btn")
+            Icon(
+                painter = feeRate.uiIcon(),
+                contentDescription = null,
+                tint = when {
+                    isDisabled -> Colors.Gray3
+                    feeRate != FeeRate.CUSTOM -> Colors.Brand
+                    else -> Color.Unspecified
+                },
+                modifier = Modifier.size(32.dp),
             )
-            VerticalSpacer(16.dp)
+            HorizontalSpacer(16.dp)
+            Column {
+                BodyMSB(feeRate.uiTitle(), color = color)
+                BodySSB(feeRate.uiDescription(), color = accent)
+            }
+            FillWidth()
+            if (sats != 0L) {
+                Column(
+                    horizontalAlignment = Alignment.End,
+                ) {
+                    MoneyMSB(sats, color = color, accent = accent)
+                    MoneySSB(sats, unit = unit.not(), color = accent, accent = accent)
+                }
+            }
         }
     }
 }
