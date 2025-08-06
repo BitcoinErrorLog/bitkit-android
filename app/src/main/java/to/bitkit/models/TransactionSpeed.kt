510ee36be726cc4f4447e47a861369960bb64e49
Ovi Trif <ovitrif@proton.me>
2025-08-06 15:02:30 +0200

    refactor: extract FeeRate

--- app/src/main/java/to/bitkit/models/TransactionSpeed.kt
+++ app/src/main/java/to/bitkit/models/TransactionSpeed.kt
@@ -1,11 +1,6 @@
 package to.bitkit.models
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
@@ -15,7 +10,6 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import to.bitkit.R
-import to.bitkit.ui.theme.Colors
 
 @Serializable(with = TransactionSpeedSerializer::class)
 sealed class TransactionSpeed {
@@ -70,57 +64,5 @@ fun TransactionSpeed.transactionSpeedUiText(): String {
         is TransactionSpeed.Medium -> stringResource(R.string.settings__fee__normal__value)
         is TransactionSpeed.Slow -> stringResource(R.string.settings__fee__slow__value)
         is TransactionSpeed.Custom -> stringResource(R.string.settings__fee__custom__value)
-    }
-}
-
-enum class FeeRate(
-    @StringRes val title: Int,
-    @StringRes val description: Int,
-    @DrawableRes val icon: Int,
-    val color: Color,
-) {
-    FAST(
-        title = R.string.fee__fast__title,
-        description = R.string.fee__fast__description,
-        color = Colors.Brand,
-        icon = R.drawable.ic_speed_fast,
-    ),
-    NORMAL(
-        title = R.string.fee__normal__title,
-        description = R.string.fee__normal__description,
-        color = Colors.Brand,
-        icon = R.drawable.ic_speed_normal,
-    ),
-    SLOW(
-        title = R.string.fee__slow__title,
-        description = R.string.fee__slow__description,
-        color = Colors.Brand,
-        icon = R.drawable.ic_speed_slow,
-    ),
-    CUSTOM(
-        title = R.string.fee__custom__title,
-        description = R.string.fee__custom__description,
-        color = Colors.White64,
-        icon = R.drawable.ic_settings,
-    );
-
-    fun toSpeed(): TransactionSpeed {
-        return when (this) {
-            FAST -> TransactionSpeed.Fast
-            NORMAL -> TransactionSpeed.Medium
-            SLOW -> TransactionSpeed.Slow
-            CUSTOM -> TransactionSpeed.Custom(0u)
-        }
-    }
-
-    companion object {
-        fun fromSpeed(speed: TransactionSpeed): FeeRate {
-            return when (speed) {
-                is TransactionSpeed.Fast -> FAST
-                is TransactionSpeed.Medium -> NORMAL
-                is TransactionSpeed.Slow -> SLOW
-                is TransactionSpeed.Custom -> CUSTOM
-            }
-        }
     }
 }
