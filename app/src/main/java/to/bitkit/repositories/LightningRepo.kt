6305b4f60fa19e4e450fa037aa7bf0c8487e35b7
Joao Victor Sena <jvsena16@gmail.com>
2025-09-04 10:33:18 -0300

    chore: lint

--- app/src/main/java/to/bitkit/repositories/LightningRepo.kt
+++ app/src/main/java/to/bitkit/repositories/LightningRepo.kt
@@ -240,7 +240,7 @@ class LightningRepo @Inject constructor(
     }
 
     /**Updates the shouldBlockLightning state and returns the current value*/
-    private suspend fun updateGeoBlockState() : Boolean {
+    private suspend fun updateGeoBlockState(): Boolean {
         val shouldBlock = coreService.shouldBlockLightning()
         _lightningState.update {
             it.copy(shouldBlockLightning = shouldBlock)
