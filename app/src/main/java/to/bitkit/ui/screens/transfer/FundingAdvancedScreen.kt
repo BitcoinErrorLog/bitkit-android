44186bd212855fb6a4ea1504511f84f88968f2e9
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-09-08 15:06:55 +0200

    Add missing FundManual test tag

--- app/src/main/java/to/bitkit/ui/screens/transfer/FundingAdvancedScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/transfer/FundingAdvancedScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -79,6 +80,7 @@ fun FundingAdvancedScreen(
                         )
                     },
                     onClick = onManual,
+                    modifier = Modifier.testTag("FundManual")
                 )
             }
         }
