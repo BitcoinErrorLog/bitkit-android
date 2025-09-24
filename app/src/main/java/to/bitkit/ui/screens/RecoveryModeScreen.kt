bada9b30a02ea10f17f1d5fde0062b4952a00c45
jvsena42 <jvsena16@gmail.com>
2025-09-24 10:34:55 -0300

    feat: layout

---
+++ app/src/main/java/to/bitkit/ui/screens/RecoveryModeScreen.kt
@@ -0,0 +1,65 @@
+package to.bitkit.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import to.bitkit.R
+import to.bitkit.ui.components.BodyM
+import to.bitkit.ui.components.PrimaryButton
+import to.bitkit.ui.components.VerticalSpacer
+import to.bitkit.ui.scaffold.AppTopBar
+import to.bitkit.ui.scaffold.ScreenColumn
+import to.bitkit.ui.theme.AppThemeSurface
+
+@Composable
+fun RecoveryModeScreen() {
+    ScreenColumn(
+        modifier = Modifier.padding(horizontal = 16.dp)
+    ) {
+        AppTopBar(
+            titleText = stringResource(R.string.security__recovery),
+            onBackClick = null,
+        )
+
+        VerticalSpacer(16.dp)
+
+        BodyM(text = stringResource(R.string.security__recovery_text))
+
+        VerticalSpacer(32.dp)
+
+        Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            PrimaryButton(
+                text = stringResource(R.string.lightning__export_logs),
+                onClick = {},
+            )
+
+            PrimaryButton(
+                text = stringResource(R.string.security__display_seed),
+                onClick = {},
+            )
+
+            PrimaryButton(
+                text = stringResource(R.string.security__contact_support),
+                onClick = {},
+            )
+
+            PrimaryButton(
+                text = stringResource(R.string.security__wipe_app),
+                onClick = {},
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        RecoveryModeScreen()
+    }
+}
