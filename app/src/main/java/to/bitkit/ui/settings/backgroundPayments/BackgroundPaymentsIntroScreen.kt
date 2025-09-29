f4355e2d8371fe94196fac6e1d7f06e7f54d0bc5
jvsena42 <jvsena16@gmail.com>
2025-09-29 10:34:27 -0300

    feat: BackgroundPaymentsIntroScreen.kt

---
+++ app/src/main/java/to/bitkit/ui/settings/backgroundPayments/BackgroundPaymentsIntroScreen.kt
@@ -0,0 +1,81 @@
+package to.bitkit.ui.settings.backgroundPayments
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import to.bitkit.R
+import to.bitkit.ui.components.BodyM
+import to.bitkit.ui.components.Display
+import to.bitkit.ui.components.PrimaryButton
+import to.bitkit.ui.scaffold.AppTopBar
+import to.bitkit.ui.scaffold.CloseNavIcon
+import to.bitkit.ui.shared.util.screen
+import to.bitkit.ui.theme.AppThemeSurface
+import to.bitkit.ui.theme.Colors
+import to.bitkit.ui.utils.withAccent
+
+@Composable
+fun BackgroundPaymentsIntroScreen(
+    onBack: () -> Unit,
+    onClose: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.screen()
+    ) {
+        AppTopBar(
+            titleText = "Background Payments", // Todo Transifex
+            onBackClick = onBack,
+            actions = { CloseNavIcon(onClick = onClose) },
+        )
+        BackgroundPaymentsIntroContent(onContinue = onBack)
+    }
+}
+
+@Composable
+fun BackgroundPaymentsIntroContent(onContinue: () -> Unit) {
+    Column(
+        modifier = Modifier.padding(horizontal = 32.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bell),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        )
+
+        Display(
+            text = "GET PAID\n<accent>PASSIVELY</accent>".withAccent(accentColor = Colors.Blue),
+            color = Colors.White
+        )
+        Spacer(Modifier.height(8.dp))
+        BodyM(text = "Turn on notifications to get paid, even when your Bitkit app is closed.", color = Colors.White64)
+        Spacer(Modifier.height(32.dp))
+        PrimaryButton(
+            text = stringResource(R.string.common__continue),
+            onClick = onContinue,
+            modifier = Modifier.testTag("BackgroundPaymentsIntro-button")
+        )
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        BackgroundPaymentsIntroContent(
+            onContinue = {}
+        )
+    }
+}
