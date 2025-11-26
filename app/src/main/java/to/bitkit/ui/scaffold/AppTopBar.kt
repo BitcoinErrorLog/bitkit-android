535ddd61eee5d558bcb3e8d0260dac1936ecec28
jvsena42 <jvsena16@gmail.com>
2025-11-26 09:32:38 -0300

    chore: preview visibility

--- app/src/main/java/to/bitkit/ui/scaffold/AppTopBar.kt
+++ app/src/main/java/to/bitkit/ui/scaffold/AppTopBar.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -93,12 +94,13 @@ fun BackNavIcon(
 fun DrawerNavIcon(
     modifier: Modifier = Modifier,
 ) {
+    val isPreview = LocalInspectionMode.current
     val drawerState = LocalDrawerState.current
     val scope = androidx.compose.runtime.rememberCoroutineScope()
 
-    if (drawerState != null) {
+    if (drawerState != null || isPreview) {
         IconButton(
-            onClick = { scope.launch { drawerState.open() } },
+            onClick = { scope.launch { drawerState?.open() } },
             modifier = modifier.testTag("HeaderMenu")
         ) {
             Icon(
