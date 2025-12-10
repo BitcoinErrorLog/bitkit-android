3e9a8d0db9b1417f31d4bd9bc1ce77526ddc43a7
jvsena42 <jvsena16@gmail.com>
2025-12-10 07:42:59 -0300

    chore: lint

--- app/src/main/java/to/bitkit/ui/screens/wallets/HomeViewModel.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/HomeViewModel.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
